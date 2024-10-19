cc6684fd77272f1e57212daae57091e468cc8a33
FireGreeks <theodorbischler1@gmail.com>
2024-10-19 12:32:23 +0200

    Added package.json

--- index.ts
+++ index.ts
@@ -2,10 +2,8 @@ import ApiRequestImpl from "./api-fetch/api-request";
 import {FilterChain} from "./api-filter-chain/filter-chain";
 import {ApiOptions} from "./api-fetch/api-options";
 import filterPresets from "./api-filter-chain/filter-presets";
-
-
 export default {
-  get: <T = Response>(url: string, responseFilterChain: FilterChain<Response, any, T>, options?: ApiOptions<T>): Promise<T> => {
+  get: <T>(url: string, responseFilterChain: FilterChain<Response, any, T>, options?: ApiOptions<T>): Promise<T> => {
     const request: ApiRequestImpl<T> = new ApiRequestImpl<T>(url, 'GET', responseFilterChain, options)
     return request.fetch()
   },
