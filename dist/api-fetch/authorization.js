f73e45597b1a6b24ce6817f5f84b77ebdc43584c
FireGreeks <theodorbischler1@gmail.com>
2025-04-16 17:07:27 +0200

    Changes: - Removed unnecessary to encode JWT token on bearer authorization - Built latest version

--- dist/api-fetch/authorization.js
+++ dist/api-fetch/authorization.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Authorization = void 0;
 exports.Authorization = {
-    Bearer: (token) => `Bearer ${btoa(token)}`,
+    Bearer: (token) => `Bearer ${token}`,
     Basic: (user, password) => `Basic ${btoa(`${user}:${password}`)}`
 };
--- src/api-fetch/authorization.ts
+++ src/api-fetch/authorization.ts
@@ -2,6 +2,6 @@
 export type AuthorizationHeader = `Bearer ${string}` | `Basic ${string}`
 
 export const Authorization = {
-    Bearer: (token: string): AuthorizationHeader => `Bearer ${btoa(token)}`,
+    Bearer: (token: string): AuthorizationHeader => `Bearer ${token}`,
     Basic: (user: string, password: string): AuthorizationHeader => `Basic ${btoa(`${user}:${password}`)}`
 }
